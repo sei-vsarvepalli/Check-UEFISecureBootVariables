12669fa7e437c05aeecb59b0294630ad8fb669b5
Vijay Sarvepalli <vss@cert.org>
2026-03-17 19:59:34 -0400

    Update Get-AuthenticodeSha256Hex implementation

--- ps/Get-AuthenticodeHash.ps1
+++ ps/Get-AuthenticodeHash.ps1
@@ -1,1 +1,24 @@
-function Get-AuthenticodeSha256Hex {  param([string]$filePath)  # Load PE file  $peFile = [System.IO.File]::OpenRead($filePath)  try {    $reader = New-Object System.IO.BinaryReader($peFile)    # Read the PE header and load necessary data    # Hashing all bytes except for certificate table blob    # Implementation details go here...    # Compute SHA256    $sha256 = [system.security.cryptography.sha256]::Create()    # Finalize the hash and return    return [BitConverter]::ToString($hash).Replace('-','').ToLower()  } finally {    $reader.Close()    $peFile.Close()  }}
+function Get-AuthenticodeSha256Hex {
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$peFile
+    )
+
+    $peHeader = [System.IO.File]::ReadAllBytes($peFile)
+    $optionalHeaderOffset = [BitConverter]::ToUInt32($peHeader, 60) + 24  # Offset of Optional Header
+    # Zero the checksum
+    [BitConverter]::GetBytes(0) | ForEach-Object { $peHeader[$optionalHeaderOffset + 88 + $_] = 0 }
+    # Zero IMAGE_DIRECTORY_ENTRY_SECURITY
+a[$optionalHeaderOffset + 96] = 0
+    a[$optionalHeaderOffset + 97] = 0
+    a[$optionalHeaderOffset + 98] = 0
+    a[$optionalHeaderOffset + 99] = 0
+    a[$optionalHeaderOffset + 100] = 0
+    a[$optionalHeaderOffset + 101] = 0
+    a[$optionalHeaderOffset + 102] = 0
+    a[$optionalHeaderOffset + 103] = 0
+
+    $hash = [System.Security.Cryptography.SHA256]::Create()
+    $hashResult = $hash.ComputeHash($peHeader)
+    return [BitConverter]::ToString($hashResult).Replace('-','').ToLower()
+}
